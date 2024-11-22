e4bedc5fd2822e518c6fe831dfe9154d7f183e72
WornDownSmoothPebble <extraforsurvey@gmail.com>
2024-11-22 14:25:32 -0800

    fix create user

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,94 +1,109 @@
 // NavBar.tsx
 import React, { useState } from 'react';
-import settingsButton from '../../images/settingsbutton.svg'
-import CreateUserModal from '../UserLogins/CreateUserModalOLD';
-import './NavBar.css'
+import settingsButton from '../../images/settingsbutton.svg';
+// import CreateUserModal from '../UserLogins/CreateUserModalOLD';
+import CreateUserModal from '../UserLogins/CreateUserModal';
+import './NavBar.css';
 
 const NavBar: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [sideMenuActive, setSideMenuActive] = useState(false);
+  const [loginModalOpen, setLoginModalOpen] = useState<boolean>(false);
+  const [newUserModalOpen, setNewUserModalOpen] = useState<boolean>(false);
+  const [sideMenuActive, setSideMenuActive] = useState<boolean>(false);
 
   const handleSideMenuToggle = () => {
     setSideMenuActive(!sideMenuActive);
   };
 
+  const createAccountModalToggle = () => {
+    console.log('toggle login modal');
+    setNewUserModalOpen(!newUserModalOpen);
+  };
 
   return (
-    <header className="header">
-         {/* Logo  */}
-        <img className="logo gglogostretchedgg" src="/images/gglogostretchedgg-2@2x.png" alt="GameGains Logo" />
-        
-         {/* Search Bar  */}
+    <>
+      <header className="header">
+        {/* Logo  */}
+        <img
+          className="logo gglogostretchedgg"
+          src="/images/gglogostretchedgg-2@2x.png"
+          alt="GameGains Logo"
+        />
+
+        {/* Search Bar  */}
         <div className="search-container">
-            <input type="text" className="search-bar" placeholder="Search"/>
-            <svg className="search-icon" viewBox="0 0 24 24">
-                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-            </svg>
+          <input type="text" className="search-bar" placeholder="Search" />
+          <svg className="search-icon" viewBox="0 0 24 24">
+            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+          </svg>
         </div>
-        
-         {/* Hamburger Menu Button  */}
+
+        {/* Hamburger Menu Button  */}
+        {/*TODO: Make this its own component*/}
         <div className="menu-btn" onClick={handleSideMenuToggle}>
-            <span></span>
-            <span></span>
-            <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-         {/* Navigation Tabs  */}
-        <div className={`nav-tabs ${sideMenuActive ? '' : ' hidden'}`} >
-             {/* First group  */}
-            <div className="tab-group">
-                <div className="tab">
-                    TEMP
-                </div>
-            </div>
+        {/* Navigation Tabs  */}
+        <div className={`nav-tabs ${sideMenuActive ? '' : ' hidden'}`}>
+          {/* First group  */}
+          <div className="tab-group">
+            <div className="tab">TEMP</div>
+          </div>
 
-             {/* Second group  */}
-            <div className="tab-group">
-                <div className="tab">
-                    TEMP
-                </div>
-            </div>
+          {/* Second group  */}
+          <div className="tab-group">
+            <div className="tab">TEMP</div>
+          </div>
 
-             {/* Third group  */}
-            <div className="tab-group">
-              <div className="tab">
-                TEMP
-              </div>
-            </div>
+          {/* Third group  */}
+          <div className="tab-group">
+            <div className="tab">TEMP</div>
+          </div>
         </div>
 
-         {/* Right Side Controls  */}
+        {/* Right Side Controls  */}
         <div className="right-controls">
-            <div className="login-btn">Login</div>
-            <button className="create-account-btn">Create Account</button>
-            
-             {/* Settings Button  TODO*/}
-            <div className="utility-btn settings-btn">
-                <svg viewBox="0 0 24 24">
-                    <path d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5-0.38,1.03-0.7,1.62-0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"/>
-                </svg>
-            </div>
+          <button className="login-btn">Login</button>
+          <button
+            className="create-account-btn"
+            onClick={createAccountModalToggle}
+          >
+            Create Account
+          </button>
 
-            
-             {/* Language Selector  TODO */}
-            <div className="utility-btn language-selector">
-                <svg viewBox="0 0 24 24">
-                    <path d="M12.87,15.07l-2.54-2.51l0.03-0.03C11.29,11.46,12,10.02,12,8.5C12,5.91,9.09,3,6.5,3S1,5.91,1,8.5S3.91,14,6.5,14 c1.52,0,2.96-0.71,4.03-1.64l0.03,0.03l2.51,2.54L12.87,15.07z M6.5,12C5.01,12,3,9.99,3,8.5S5.01,5,6.5,5S10,7.01,10,8.5 S7.99,12,6.5,12z"/>
-                </svg>
-                <div className="language-dropdown">
-                    <div className="language-option">English</div>
-                    <div className="language-option">Spanish</div>
-                    <div className="language-option">Chinese</div>
-                    <div className="language-option">Hindi</div>
-                    <div className="language-option">Arabic</div>
-                    <div className="language-option">Portuguese</div>
-                    <div className="language-option">Russian</div>
-                    <div className="language-option">Japanese</div>
-                    <div className="language-option">German</div>
-                    <div className="language-option">French</div>
-                </div>
+          {/* Settings Button  TODO*/}
+          <div className="utility-btn settings-btn">
+            <svg viewBox="0 0 24 24">
+              <path d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5-0.38,1.03-0.7,1.62-0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z" />
+            </svg>
+          </div>
+
+          {/* Language Selector  TODO */}
+          <div className="utility-btn language-selector">
+            <svg viewBox="0 0 24 24">
+              <path d="M12.87,15.07l-2.54-2.51l0.03-0.03C11.29,11.46,12,10.02,12,8.5C12,5.91,9.09,3,6.5,3S1,5.91,1,8.5S3.91,14,6.5,14 c1.52,0,2.96-0.71,4.03-1.64l0.03,0.03l2.51,2.54L12.87,15.07z M6.5,12C5.01,12,3,9.99,3,8.5S5.01,5,6.5,5S10,7.01,10,8.5 S7.99,12,6.5,12z" />
+            </svg>
+            <div className="language-dropdown">
+              <div className="language-option">English</div>
+              <div className="language-option">Spanish</div>
+              <div className="language-option">Chinese</div>
+              <div className="language-option">Hindi</div>
+              <div className="language-option">Arabic</div>
+              <div className="language-option">Portuguese</div>
+              <div className="language-option">Russian</div>
+              <div className="language-option">Japanese</div>
+              <div className="language-option">German</div>
+              <div className="language-option">French</div>
             </div>
+          </div>
         </div>
-    </header>
+      </header>
+      <CreateUserModal
+        modalToggle={createAccountModalToggle}
+        createOpen={newUserModalOpen}
+      />
+    </>
   );
 };
 
--- src/components/UserLogins/CreateUserModal.tsx
+++ src/components/UserLogins/CreateUserModal.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 
 interface CreateUserModalProps {
-  onClose: () => void;
+  modalToggle: () => void;
+  createOpen: boolean;
 }
 
-const CreateUserModal: React.FC<CreateUserModalProps> = ({ onClose }) => {
+const CreateUserModal: React.FC<CreateUserModalProps> = (
+  props: CreateUserModalProps
+) => {
   const [username, setUsername] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
 
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
-  console.log('backend url ', backendUrl);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     console.log('Username:', username);
     console.log('Password:', password);
     console.log('email', email);
@@ -21,14 +23,18 @@ const CreateUserModal: React.FC<CreateUserModalProps> = ({ onClose }) => {
 
     try {
       const response = await fetch(`http://localhost:3000/users/create`, {
-        method: 'POST', // Specify the method
+        method: 'POST',
         headers: {
-          'Content-Type': 'application/json', // Specify content type
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ "username": username, password: "password", email: "email"}),
+        body: JSON.stringify({
+          name: username,
+          password: password,
+          email: email,
+        }),
       });
 
-      console.log('response', response)
+      console.log('response', response);
 
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -36,51 +42,105 @@ const CreateUserModal: React.FC<CreateUserModalProps> = ({ onClose }) => {
 
       const data = await response.json(); // Parse the response data
       console.log('User created:', data); // Handle the response data (e.g., success message)
-      onClose(); // Close the modal after successful submission
+      props.modalToggle(); // Close the modal after successful submission
     } catch (error) {
       console.error('Error creating user:', error);
     }
-    onClose(); // Close the modal after submission
+    props.modalToggle(); // Close the modal after submission
   };
 
-  return (
-    <div className="modal-overlay" id="createAccountModal">
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  // TODO: THIS THING
+  const handleRepeatPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRepeatPassword(event.target.value);
+  };
+
+  if (props.createOpen) {
+    return (
+      <div className="modal-overlay" id="createAccountModal">
         <div className="modal-container">
-            <div className="modal-close" id="modalClose">
-                <svg viewBox="0 0 24 24">
-                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12z"/>
-                </svg>
+          <div className="modal-close" id="modalClose" onClick={props.modalToggle}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+          <div className="form-header">
+            <img
+              className="gglogostretchedgg"
+              src="/images/gglogostretchedgg-2@2x.png"
+              alt="GameGains Logo"
+            />
+          </div>
+          <form id="createAccountForm">
+            <div className="form-group">
+              <label htmlFor="email">Username</label>
+              <input id="email" name="username" required onChange={handleUsernameChange}/>
+              <div className="error-message" id="emailError"></div>
             </div>
-            <div className="form-header">
-                <img className="gglogostretchedgg" src="https://c.animaapp.com/EtqOPAmu/img/gglogostretchedgg-2@2x.png" alt="GameGains Logo" />
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required onChange={handleEmailChange}/>
+              <div className="error-message" id="emailError"></div>
             </div>
-            <form id="createAccountForm">
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" required/>
-                    <div className="error-message" id="emailError"></div>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" required/>
-                    <div className="error-message" id="passwordError"></div>
-                    <div className="password-requirements">
-                        Password must be at least 8 characters long and include a number and special character
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" id="confirmPassword" name="confirmPassword" required/>
-                    <div className="error-message" id="confirmPasswordError"></div>
-                </div>
-                <button type="submit" className="submit-btn">Create Account</button>
-            </form>
-            <div className="login-link">
-                Already have an account?<a href="#" id="switchToLogin">Login</a>
+            {/* TODO: Handle repeat passwords and the guidelines */}
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required onChange={handlePasswordChange}/>
+              <div className="error-message" id="passwordError"></div>
+              <div className="password-requirements">
+                Password must be at least 8 characters long and include a number
+                and special character (NOT HANDLED RN)
+              </div>
             </div>
+            {/* <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                required
+              />
+              <div className="error-message" id="confirmPasswordError"></div>
+            </div> */}
+            <button type="submit" className="submit-btn" onClick={handleSubmit}>
+              Create Account
+            </button>
+          </form>
+          {/* TODO: FIX THIS */}
+          <div className="login-link">
+            Already have an account?
+            <a href="#" id="switchToLogin">
+              Login (NOT HANDLED RN)
+            </a>
+          </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default CreateUserModal;
