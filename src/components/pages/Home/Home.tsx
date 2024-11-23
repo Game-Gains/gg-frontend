d623ae12663cebcdede24cf3fcb2d3351557e570
WornDownSmoothPebble <extraforsurvey@gmail.com>
2024-11-22 18:12:50 -0800

    add back scrolling ticker for example

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -6,6 +6,24 @@ const Home: React.FC = () => {
   // TODO: BRING EVERYTHING OVER FROM THE PURE HTML VERSION
   return (
     <div className="home-container">
+      <div className="ticker-wrap">
+        {/* TODO: MAKE THIS LIVE  */}
+            <div className="ticker">
+                <div className="ticker-item">🔥 TRENDING MEMECOINS:</div>
+                <div className="ticker-item">PEPE +15.2% ($0.000003912)</div>
+                <div className="ticker-item">WOJAK +8.7% ($0.00000891)</div>
+                <div className="ticker-item">BONK +22.4% ($0.00001234)</div>
+                <div className="ticker-item">DOGE +5.6% ($0.12832)</div>
+                <div className="ticker-item">SHIB +10.3% ($0.00002891)</div>
+                {/* <!-- Duplicate items for seamless loop --> */}
+                <div className="ticker-item">🔥 TRENDING MEMECOINS:</div>
+                <div className="ticker-item">PEPE +15.2% ($0.000003912)</div>
+                <div className="ticker-item">WOJAK +8.7% ($0.00000891)</div>
+                <div className="ticker-item">BONK +22.4% ($0.00001234)</div>
+                <div className="ticker-item">DOGE +5.6% ($0.12832)</div>
+                <div className="ticker-item">SHIB +10.3% ($0.00002891)</div>
+            </div>
+        </div>
       <h1 className='headheadhead'>TEMPORARY Home Page</h1>
       <Link to="/roulette" className="headheadhead">
         LINK TO GO TO ROULETTE
